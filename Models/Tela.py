a4ed403e3a5f8371bb38739e0093ec8f8097e84c
GSLimaDTS <gslima.dts@gmail.com>
2021-03-31 19:57:08 -0300

    Implementando perda de pontos e criando funcoes de tela e implementando

---
+++ Models/Tela.py
@@ -0,0 +1,19 @@
+def nivel_dificuldade() -> None:
+    print('Informe o nivel de dificuldade desejado:[1, 2, 3, 4]: ')
+
+
+def informe_resultado_operacao() -> None:
+    print('Informe o resultado da seguinte operacao: ')
+
+
+def mostar_pontos(pontos) -> None:
+    print(f'Voce Tem {pontos} ponto(s)')
+
+
+def escolha_continuar() -> None:
+    print('Deseja continuar jogando? [1 = sim, 0 = nao]: ')
+
+
+def mensagem_encerramneto(pontos) -> None:
+    print(f'Voce finalizou o jogo com {pontos} ponto(s)')
+    print('Ate a proxima!')
